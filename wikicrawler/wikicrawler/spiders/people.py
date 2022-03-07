c4c6fa8373146c4e25eb4477a14365a225ebfd9b
Tyton25 <tybenz1@gmail.com>
2022-03-06 17:23:04 -0700

    Gets infobox vcard regex search working for 'infobox vcard' and 'infobox biography vcard' instances.

--- wikicrawler/wikicrawler/spiders/people.py
+++ wikicrawler/wikicrawler/spiders/people.py
@@ -1,5 +1,7 @@
 import scrapy
+from scrapy import Selector
 from collections import defaultdict
+import re
 
 MY_URL_BASE = "en.wikipedia.org/wiki/"
 DEFAULT_PROPS = ['born',
@@ -9,27 +11,50 @@ DEFAULT_PROPS = ['born',
                  'parents',
                  'occupation',
                  'relatives']
+EDUCATION_TYPE = ["education", "college", "alma mater"]
+NBSP = "&nbsp;"
+
+"""Regular Expressions"""
+VCARD_TABLE_CLASS = re.compile(r'infobox.*vcard')
 
 
 class PeopleSpider(scrapy.Spider):
     name = 'people'
     allowed_domains = ['en.wikipedia.org/wiki/']
     start_urls = ['http://en.wikipedia.org/wiki/Robert_Reich']
+    # start_urls = ['http://en.wikipedia.org/wiki/Yuval_Noah_Harari']
 
     def parse(self, response):
-        my_infobox_trs = response.xpath('//table[@class="infobox vcard"]/tbody/tr')
-        # my_infobox_ths = my_infobox.xpath('th')
-        # my_infobox_tds = my_infobox.xpath('td')
+        # print(response.xpath('//table[contains(@class,"vcard"]'))
+        table_classes = response.css('.vcard').xpath("@class").extract()
+        for cls in table_classes:
+            match = re.search(VCARD_TABLE_CLASS, cls)
+            if match:
+                print("Class = {}".format(match.group(0)))
+                table_cls = match.group(0)
+            else:
+                print("No Table Classes match vcard.")
+
+        table_cls_str = "//table[@class='{}']/tbody/tr".format(table_cls)
+        print(table_cls_str)
+        my_infobox_trs = response.xpath(table_cls_str)
+
         people_dict = defaultdict()
         people_dict['name'] = my_infobox_trs[0].xpath('th/div/text()').get()
 
         for tr in my_infobox_trs:
             if tr.xpath('th'):
                 if tr.xpath('th/text()').get() not in [None, '']:
-                    title = tr.xpath('th/text()').get()
+                    label = tr.xpath('th/text()').get().lower()
+                    # label = label_raw.replace(NBSP, " ")
+                    print(label)
+                    if label in EDUCATION_TYPE:
+                        print("####### EDUCATION LIST #######")
+                        education_list = tr.xpath('td')
+                        self.get_education(education_list)
                     if tr.xpath('th/a/text()').get():
-                        title2 = tr.xpath('th/a/text()').get()
-                        title += title2
+                        label2 = tr.xpath('th/a/text()').get()
+                        label += label2
                     # print(title)
                     # people_dict[title] = None
                 if tr.xpath('td'):
@@ -39,6 +64,15 @@ class PeopleSpider(scrapy.Spider):
                         # print(tr.getAttribute('href'))
                     else:
                         val = tr.xpath('td/text()').get()
-                    # print(val)
-                    people_dict[title] = val
+                    people_dict[label] = val
         print(people_dict)
+
+    def get_education(self, edu_list):
+        # print(edu_list)
+        for ed in edu_list:
+            if ed.xpath('a'):
+                print(len(ed.xpath('a')))
+                # for school in ed.xpath('a'):
+                #     print(school.xpath('text()').get())
+            if ed.xpath('span'):
+                print(ed.xpath('span/a/text()').get())
